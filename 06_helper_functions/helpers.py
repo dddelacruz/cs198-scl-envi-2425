496ac1ad0b13af876c6d9313720119104f82f6c1
dddelacruz <dddelacruz7@up.edu.ph>
2025-05-15 17:11:06 +0800

    refactor: added helper functions for getting list of municipalities

---
+++ 06_helper_functions/helpers.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+from pathlib import Path
+
+def get_prov_code(province):
+    script_location = Path(__file__).absolute().parent
+    file_location = script_location / '../01_data/00_raw/02_psgc_codes/PH_Adm2_ProvDists.csv'
+
+    # load csv file
+    df_psgc_prov = pd.read_csv(file_location)
+
+    # get code for province
+    prov_psgc = df_psgc_prov.loc[df_psgc_prov['adm2_en'] == province].iloc[0]['adm2_psgc']
+
+    # delete dataframe
+    del df_psgc_prov
+
+    return prov_psgc
+
+def get_mun_codes(prov_psgc):
+    script_location = Path(__file__).absolute().parent
+    file_location = script_location / '../01_data/00_raw/02_psgc_codes/PH_Adm3_MuniCities.csv'
+
+    # load csv file
+    df_psgc_mun = pd.read_csv(file_location)
+
+    # filter dataframe
+    df_psgc_mun = df_psgc_mun[df_psgc_mun['adm2_psgc'] == prov_psgc]
+    mun_pgsc = df_psgc_mun['adm3_psgc'].copy()
+
+    # delete dataframe
+    del df_psgc_mun
+
+    return mun_pgsc
