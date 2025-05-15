d09cf22609ed91af682b554a6f3192098395b066
dddelacruz <dddelacruz7@up.edu.ph>
2025-05-15 16:07:34 +0800

    chore: moved ModelAnalysis.R to SpatioTemporal.R

--- 03_r_scripts/SpatioTemporal.R
+++ 03_r_scripts/SpatioTemporal.R
@@ -321,6 +321,26 @@ shapefile@data$typeIV <- mod_IV$summary.fitted.values[, "mean"]
 # export results
 write.csv(shapefile@data, str_glue("../04_results/{province}/01_spatiotemporal/{province}_results.csv"))
 
+
+# get waic and dic
+mod_criteria <- data.frame(
+  Name=character(),
+  WAIC=double(),
+  DIC=double()
+)
+
+mod_criteria <- mod_criteria %>% 
+  add_row(Name = "Base", WAIC = mod_base$waic$waic, DIC = mod_base$dic$dic) %>% 
+  add_row(Name = "TypeI", WAIC = mod_I$waic$waic, DIC = mod_I$dic$dic)%>% 
+  add_row(Name = "TypeII", WAIC = mod_II$waic$waic, DIC = mod_II$dic$dic)%>% 
+  add_row(Name = "TypeIII", WAIC = mod_III$waic$waic, DIC = mod_III$dic$dic)%>% 
+  add_row(Name = "TypeIV", WAIC = mod_IV$waic$waic, DIC = mod_IV$dic$dic)
+
+
+# save waic and dic per model
+write.csv(mod_criteria, str_glue("../04_results/{province}/01_spatiotemporal/{province}_model_criteria.csv"))
+
+
 # save models
 save(mod_base, file = str_glue("../04_results/{province}/01_spatiotemporal/models/{province}_base.Rda"))
 save(mod_I, file = str_glue("../04_results/{province}/01_spatiotemporal/models/{province}_typeI.Rda"))
